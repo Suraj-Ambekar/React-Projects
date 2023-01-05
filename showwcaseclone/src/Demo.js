ed6b9be093bc65627fefb4963a33b9791ba27294
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-01-05 10:30:13 +0530

    successfully created second containers first row design

--- showwcaseclone/src/Demo.js
+++ showwcaseclone/src/Demo.js
@@ -23,14 +23,24 @@ export default function Demo(){
                     <button className="add"><i class="fa-solid fa-plus"></i></button>
                 </div>
                 <div className="home">
-                        <a href="www.google.com"><i class="fa-solid fa-house"></i><span className="active">Home</span></a>
-                        <a href="www.google.com"><i class="fa-solid fa-newspaper"></i><span>Shows</span></a>
-                        <a href="www.google.com"><i class="fa-sharp fa-solid fa-users"></i><span>Circles</span></a>
-                        {/* <a href="www.google.com"><i class="fa-regular fa-circle-star"></i><span>Circles</span></a> */}
-                        <a href="www.google.com"><i class="fa-solid fa-briefcase"></i><span>Work</span></a>
-                        <a href="www.google.com"><i class="fa-solid fa-bookmark"></i><span>Bookmarks</span></a>
-                        <a href="www.google.com"><i class="fa-solid fa-boxes-stacked space"></i><span>AllCommunities</span></a>
-                        <hr className="hr" color="gray" />
+                    <a href="www.google.com"><i class="fa-solid fa-house"></i><span className="active">Home</span></a>
+                    <a href="www.google.com"><i class="fa-solid fa-newspaper"></i><span>Shows</span></a>
+                    <a href="www.google.com"><i class="fa-sharp fa-solid fa-users"></i><span>Circles</span></a>
+                    {/* <a href="www.google.com"><i class="fa-regular fa-circle-star"></i><span>Circles</span></a> */}
+                    <a href="www.google.com"><i class="fa-solid fa-briefcase"></i><span>Work</span></a>
+                    <a href="www.google.com"><i class="fa-solid fa-bookmark"></i><span>Bookmarks</span></a>
+                    <a href="www.google.com"><i class="fa-solid fa-boxes-stacked space"></i><span>AllCommunities</span></a>
+                    {/* <hr className="hr" color="gray" /> */}      
+                </div>
+                <hr className="hr" color="gray" />
+                <div className="second-container">
+                    <a href="www.google.com">
+                        <div className="cName">
+                            <div className="circle"></div>
+                            <img className="community-imgs" src="https://profile-assets.showwcase.com/137x/11145/1637849670322-100.jpg?type=webp" alt="{item.alt}" />
+                            <span className="active">Home</span>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
