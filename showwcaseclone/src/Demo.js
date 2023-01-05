171a0f74f8589cbbbae1936e7c48290e79ceb588
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-01-05 13:22:08 +0530

    created second container of sidebar

--- showwcaseclone/src/Demo.js
+++ showwcaseclone/src/Demo.js
@@ -1,17 +1,29 @@
 import './Demo.css';
-// import communites from './communities';
+import communites from './communities';
 
-// const community = communites.map(item=>{
-//     return(
-//       <div className="communities">
-//         <div className="circle"></div>
-//         {/* <div className="imgs-name"> */}
-//             <img className="community-imgs" src={item.src} alt={item.alt} />
-//         {/* </div> */}
-//         <span>{item.name}</span>
-//       </div>
-//     )
-//   })
+const community = communites.map(item=>{
+    let communityName = item.name;
+    // console.log(communityName);
+    console.log(communityName.length)
+    let changedCommunityName ="";
+
+    if (communityName.length>18){
+        changedCommunityName= communityName.slice(0,16)+"...";
+    }else{
+        changedCommunityName = communityName;
+    }
+    // console.log(b)
+
+    return(
+        <a href="www.google.com">
+            <div className="cName">
+                <div className="circle"></div>
+                <img className="community-imgs" src={item.src} alt={item.alt} />
+                <span className="active">{changedCommunityName}</span>
+            </div>
+        </a>
+    )
+  })
 
 export default function Demo(){
     
@@ -29,18 +41,19 @@ export default function Demo(){
                     {/* <a href="www.google.com"><i class="fa-regular fa-circle-star"></i><span>Circles</span></a> */}
                     <a href="www.google.com"><i class="fa-solid fa-briefcase"></i><span>Work</span></a>
                     <a href="www.google.com"><i class="fa-solid fa-bookmark"></i><span>Bookmarks</span></a>
-                    <a href="www.google.com"><i class="fa-solid fa-boxes-stacked space"></i><span>AllCommunities</span></a>
+                    <a href="www.google.com"><i class="fa-solid fa-boxes-stacked space"></i><span>All Communities</span></a>
                     {/* <hr className="hr" color="gray" /> */}      
                 </div>
                 <hr className="hr" color="gray" />
                 <div className="second-container">
-                    <a href="www.google.com">
+                    {/* <a href="www.google.com">
                         <div className="cName">
                             <div className="circle"></div>
                             <img className="community-imgs" src="https://profile-assets.showwcase.com/137x/11145/1637849670322-100.jpg?type=webp" alt="{item.alt}" />
                             <span className="active">Home</span>
                         </div>
-                    </a>
+                    </a> */}
+                    {community}
                 </div>
             </div>
         </div>
