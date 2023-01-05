96940a913a195d35eb09018a0364a1a0c7f60df2
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-01-05 09:23:54 +0530

    successfully created first section of the sidebar

--- showwcaseclone/src/Demo.js
+++ showwcaseclone/src/Demo.js
@@ -16,11 +16,21 @@ import './Demo.css';
 export default function Demo(){
     
     return(
-        <div className="sideebar">
+        <div className="sidebar">
+            <a href="www.showwcase.com"><img className="logo" src="./showwcase_logo.png" alt="showwcase logo" /></a>
             <div className="sidebar-content">
-                <a href="www.showwcase.com"><img className="logo" src="./showwcase_logo.png" alt="showwcase logo" /></a>
                 <div className="btn">
                     <button className="add"><i class="fa-solid fa-plus"></i></button>
+                </div>
+                <div className="home">
+                        <a href="www.google.com"><i class="fa-solid fa-house"></i><span className="active">Home</span></a>
+                        <a href="www.google.com"><i class="fa-solid fa-newspaper"></i><span>Shows</span></a>
+                        <a href="www.google.com"><i class="fa-sharp fa-solid fa-users"></i><span>Circles</span></a>
+                        {/* <a href="www.google.com"><i class="fa-regular fa-circle-star"></i><span>Circles</span></a> */}
+                        <a href="www.google.com"><i class="fa-solid fa-briefcase"></i><span>Work</span></a>
+                        <a href="www.google.com"><i class="fa-solid fa-bookmark"></i><span>Bookmarks</span></a>
+                        <a href="www.google.com"><i class="fa-solid fa-boxes-stacked space"></i><span>AllCommunities</span></a>
+                        <hr className="hr" color="gray" />
                 </div>
             </div>
         </div>
