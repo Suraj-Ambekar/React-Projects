036b06b4d413132f34562ca1377e84f0d1ccbb63
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-01-07 14:55:48 +0530

    added a dropdown event listener and modified css

--- showwcaseclone/src/Demo.js
+++ showwcaseclone/src/Demo.js
@@ -1,6 +1,6 @@
 import './Demo.css';
 import communites from './communities';
-import {useState} from 'react';
+// import {useState} from 'react';
 
 const community = communites.map(item=>{
     let communityName = item.name;
@@ -16,7 +16,7 @@ const community = communites.map(item=>{
     // console.log(b)
 
     return(
-        <a href="">
+        <a href="www.google.com">
             <div className="cName">
                 <div className="circle"></div>
                 <img className="community-imgs" src={item.src} alt={item.alt} />
@@ -45,7 +45,7 @@ export default function Demo(){
                     <button className="add"><i class="fa-solid fa-plus"></i></button>
                 </div>
                 <div className="home">
-                    <a href="www.google.com"><i class="fa-solid fa-house"></i><span className="active">Home</span></a>
+                    <a href="www.google.com"><i class="fa-solid fa-house"></i><span>Home</span></a>
                     <a href="www.google.com"><i class="fa-solid fa-newspaper"></i><span>Shows</span></a>
                     <a href="www.google.com"><i class="fa-sharp fa-solid fa-users"></i><span>Circles</span></a>
                     {/* <a href="www.google.com"><i class="fa-regular fa-circle-star"></i><span>Circles</span></a> */}
