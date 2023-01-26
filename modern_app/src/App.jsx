1b7716cf35ce70d1ca528f40c2e425a0f4648758
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-01-26 14:04:59 +0530

    created components

--- modern_app/src/App.jsx
+++ modern_app/src/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import styles from './style';
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <h1>Hello, World!</h1>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Navbar
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Hero
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats 
+        Business 
+        Billing 
+        CarDeal 
+        Testimonials 
+        Clients 
+        CTA 
+        Footer
+      </div>
+    </div>
+
   </div>
 );
 
