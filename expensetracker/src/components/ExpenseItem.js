07a82e8b3127836092edea1ebedaf5ca5af2fa65
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-01 07:43:13 +0530

    add some styling and change code to look good

--- expensetracker/src/components/ExpenseItem.js
+++ expensetracker/src/components/ExpenseItem.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import './ExpenseItem.css';
 
-const ExpenseItem = () => { 
-    const expenseDate = new Date(2023, 0, 31);
-    const expenseTitle = "Car Insurance";
-    const expenseAmount = 294.67;
-    return (
-        <div>
-            <div className='expense-item'>{expenseDate.toISOString()}</div>
-            <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
-            </div>
-        </div>
-    )
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className='expense-item'>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
