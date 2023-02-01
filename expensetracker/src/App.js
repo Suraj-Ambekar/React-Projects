4f5b79086e48cc731c05ce6c17eac9384800037e
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-01 09:30:19 +0530

    modified code

--- expensetracker/src/App.js
+++ expensetracker/src/App.js
@@ -1,16 +1,35 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 // import ExpenseItem from './components/ExpenseItem';
-import NewExpense from "./components/NewExpense";
+import Expenses from './components/Expenses';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <div className="expenses">
-        < NewExpense />
-      </div>
+      <Expenses items={expenses} />
     </div>
   );
 }
