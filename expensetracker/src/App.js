9adf129f233925324a05990f363f8052d7a73172
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-03 09:28:25 +0530

    created expense form to add information

--- expensetracker/src/App.js
+++ expensetracker/src/App.js
@@ -2,6 +2,7 @@
 // import './App.css';
 // import ExpenseItem from './components/ExpenseItem';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   const expenses = [
@@ -28,7 +29,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
