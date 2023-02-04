288ac1d10d5bd7f3a91902b0eb7b99b26585b9d2
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-04 08:57:53 +0530

    learned to add style components on div and how to add classname

--- additems/src/components/CourseGoals/CourseInput/CourseInput.js
+++ additems/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,48 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+// import './CourseInput.css';
+
+const FormControl = styled.div`
+
+  margin: 0.5rem 0;
+
+
+& label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+& input {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+}
+
+& input:focus {
+  outline: none;
+  background: #fad0ec;
+  border-color: #8b005d;
+}
+
+&.invalid input {
+  border-color: red;
+  background-color: #ffd7d7;
+}
+&.invali label {
+  color: red;
+}
+
+
+
+
+`;
+
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -25,10 +66,12 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      {/* <div className={`form-control ${!isValid ? 'invalid' : ''}`}> */}
+      < FormControl className={!isValid && 'invalid'}> 
         <label >Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
+      {/* </div> */}
       <Button type="submit">Add Goal</Button>
     </form>
   );
