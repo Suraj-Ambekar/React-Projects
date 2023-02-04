4da27d392176aa1a6e74e2b5770c7d7e7ae75876
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-04 09:16:34 +0530

    learned to add media queries using styled components

--- additems/src/components/UI/Button/Button.js
+++ additems/src/components/UI/Button/Button.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 
 // const Button = props => {
   const Button = styled.button`
-  
+    width: 100%;
     font: inherit;
     padding: 0.5rem 1.5rem;
     border: 1px solid #8b005d;
@@ -13,6 +13,10 @@ import styled from 'styled-components';
     background: #8b005d;
     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
     cursor: pointer;
+
+    @media (min-width: 768px) {
+      width: auto;
+    }
   
   
   &:focus {
