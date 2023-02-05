e32714ae886709d8e45b7319b87e15398882f6db
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-05 07:55:36 +0530

    completed practice project

--- practiceproject/src/App.js
+++ practiceproject/src/App.js
@@ -1,10 +1,24 @@
 // import logo from './logo.svg';
 // import './App.css';
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+import { useState } from "react";
+// import UserInfo from "./components/UserInfo";
 
 function App() {
+
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) =>{
+    setUsersList((prevUL) => {
+      return [...prevUL, {name: uName, age: uAge, id: Math.random.toString()}]
+    })
+  }
+
   return (
     <div className="App">
-      
+      < AddUser onAddUser={addUserHandler} />
+      < UsersList users={usersList} />
     </div>
   );
 }
---
+++ practiceproject/src/components/Users/UsersList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import classes from './UsersList.module.css';
+import Card from '../UI/Card';
+
+const UsersList = (props) => {
+  return (
+    < Card className={classes.users}>
+      <ul>
+        {
+          props.users.map((user) => {
+            return (
+            <li key={user.id}>
+              {user.name} ({user.age} years old)
+            </li>
+            )
+          })
+        }
+      </ul>
+    </Card>
+  )
+}
+
+export default UsersList
