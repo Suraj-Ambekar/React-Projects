ba4b236e695875806e6bb0a396f6bfc71f968755
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-06 17:49:03 +0530

    created overlay portal:

--- foodorderapp/src/App.js
+++ foodorderapp/src/App.js
@@ -1,13 +1,14 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
 
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <Fragment>
-      < Header />
+      <Cart />
+      <Header />
       <main>
         <Meals />
       </main>
