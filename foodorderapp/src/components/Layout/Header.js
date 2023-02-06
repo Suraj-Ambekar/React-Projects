77aac5b463c43fdafbfd45b5035c222955c596eb
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-06 15:56:49 +0530

    created cart button

--- foodorderapp/src/components/Layout/Header.js
+++ foodorderapp/src/components/Layout/Header.js
@@ -1,20 +1,21 @@
-import classes from "./Header.module.css";
-import {Fragment} from 'react';
-import mealsImg from '../../assets/meals.jpg'
+import { Fragment } from 'react';
 
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
-function Header(props) {
-    return (
-        <Fragment>
-            <header className={classes.header}>
-                <h1>Foodies</h1>
-                <button>Cart</button>
-            </header>
-            <div className={classes['main-image']}>
-                <img  src={mealsImg}  alt="table full of meals" />
-            </div>
-        </Fragment>
-    )
-}
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
+};
 
 export default Header;
