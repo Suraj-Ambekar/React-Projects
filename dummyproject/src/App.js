29c830dff2a1d81fdb3468338f7e6c4f659dc830
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-06 10:30:36 +0530

    learend context api

--- dummyproject/src/App.js
+++ dummyproject/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect} from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './components/store/auth-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -32,13 +33,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    < AuthContext.Provider value={{isLoggedIn : isLoggedIn,}}>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
