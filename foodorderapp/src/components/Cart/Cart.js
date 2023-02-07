daa6b9e0e4db96a4fa3eb0db3a68b1b7b1b8606c
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-07 16:48:02 +0530

    completed cart and layout component

--- foodorderapp/src/components/Cart/Cart.js
+++ foodorderapp/src/components/Cart/Cart.js
@@ -3,7 +3,7 @@ import { useContext } from 'react';
 import Modal from '../UI/Modal';
 import CartItem from './CartItem';
 import classes from './Cart.module.css';
-import CartContext from '../../store/cart-contex';
+import CartContext from '../../store/cart-context';
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
@@ -11,9 +11,13 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemAddHandler = (item) => {};
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes['cart-items']}>
