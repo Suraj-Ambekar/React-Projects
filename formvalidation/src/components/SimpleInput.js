bdb813f0539ca4c0715d180734c34bedd69db7c8
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-08 16:07:57 +0530

    learned about how to take values from input using useState and useRef

--- formvalidation/src/components/SimpleInput.js
+++ formvalidation/src/components/SimpleInput.js
@@ -1,9 +1,32 @@
+import React from 'react'
+import { useState, useRef } from 'react';
+
+
 const SimpleInput = (props) => {
+
+  const enteredValue = useRef();
+
+  const [enteredName, setEnteredName] = useState('');
+
+  function changeHandler(event) {
+    setEnteredName(event.target.value);
+  }
+
+  function onSubmitHandler(event) {
+    event.preventDefault();
+
+    console.log(enteredName);
+
+    const curr = enteredValue.current.value;
+    console.log(curr);
+    setEnteredName('')
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input value={enteredName} ref={enteredValue} type='text' id='name' onChange={changeHandler}  />
       </div>
       <div className="form-actions">
         <button>Submit</button>
