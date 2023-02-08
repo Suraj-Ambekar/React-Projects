d6028ba6d2a058d114f6efc4a1e6ec1a48feb4f8
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-08 16:21:53 +0530

    added some basic validation

--- formvalidation/src/components/SimpleInput.js
+++ formvalidation/src/components/SimpleInput.js
@@ -8,6 +8,8 @@ const SimpleInput = (props) => {
 
   const [enteredName, setEnteredName] = useState('');
 
+  const [enteredNameValid, setEnteredNameValid] = useState(true);
+
   function changeHandler(event) {
     setEnteredName(event.target.value);
   }
@@ -15,18 +17,30 @@ const SimpleInput = (props) => {
   function onSubmitHandler(event) {
     event.preventDefault();
 
+    if (enteredName.trim() === ''){
+      setEnteredNameValid(false)
+      return;
+    }
+
+    setEnteredNameValid(true);
     console.log(enteredName);
 
     const curr = enteredValue.current.value;
+    if (curr.trim() === ''){
+      return;
+    }
     console.log(curr);
     setEnteredName('')
   }
 
+  const nameInputClasses = enteredNameValid ? 'form-control' : 'form-control invalid';
+
   return (
     <form onSubmit={onSubmitHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input value={enteredName} ref={enteredValue} type='text' id='name' onChange={changeHandler}  />
+        {!enteredNameValid && <p className="error-text">Input is wrong</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
