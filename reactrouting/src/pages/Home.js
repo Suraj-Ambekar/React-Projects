daee067267a9fc8a0f62a538197088806f2af6b2
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-12 09:41:43 +0530

    learned about useNavigate hook

--- reactrouting/src/pages/Home.js
+++ reactrouting/src/pages/Home.js
@@ -1,11 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
+
+    const navigate = useNavigate();
+
+    const clickHandler = () => {
+        navigate('/products');
+    }
+
     return (
         <div>
             <div>My Home Page.</div>
             {/* <a href="/products">Products page</a> This is a wrong approach */}
             <Link to="/products">Products page</Link>
+            <p>
+                <button onClick={clickHandler}>Click here to go Products page</button>
+            </p>
         </div>
         
     )
