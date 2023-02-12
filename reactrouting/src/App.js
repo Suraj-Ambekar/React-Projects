662a50725f781c7933b36b3c52dad00328bb3fac
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-12 09:35:48 +0530

    learned how to add NavLink

--- reactrouting/src/App.js
+++ reactrouting/src/App.js
@@ -1,5 +1,6 @@
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import RootLayout from './components/Root';
+import ErrorPage from './pages/Error';
 import HomePage from "./pages/Home";
 import ProductsPage from './pages/Products';
 
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {path: '/' , element: <HomePage />},
       {path: '/products', element: <ProductsPage />}
--- reactrouting/src/components/MainNavigation.js
+++ reactrouting/src/components/MainNavigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from "./MainNavigation.module.css";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const MainNavigation = () => {
   return (
@@ -8,10 +8,21 @@ const MainNavigation = () => {
         <nav>
             <ul className={classes.list}>
                 <li>
-                    <Link to='/'>Home</Link>
+                    <NavLink 
+                        to='/' 
+                        className={({isActive}) => isActive ? classes.active : undefined}
+                        end
+                    >
+                        Home
+                    </NavLink>
                 </li>
                 <li>
-                    <Link to='/products'>Products</Link>
+                <NavLink 
+                        to='/products' 
+                        className={({isActive}) => isActive ? classes.active : undefined}
+                    >
+                        Products
+                    </NavLink>
                 </li>
             </ul>
         </nav>
