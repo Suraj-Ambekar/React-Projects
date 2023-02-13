9df07a5b9658922ee899d69c2b4293af9fc8091a
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-13 10:25:08 +0530

    added form validation

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,9 +1,10 @@
-import { useNavigate, useNavigation } from 'react-router-dom';
+import { useNavigate, useNavigation, useActionData } from 'react-router-dom';
 import { Form } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
+  const data = useActionData();
   const navigate = useNavigate();
 
   const navigation = useNavigation();
@@ -15,6 +16,13 @@ function EventForm({ method, event }) {
 
   return (
     <Form method='post' className={classes.form}>
+      {data && data.errors && (
+        <ul>
+          {Object.values(data.errors).map((err)=> (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>
+      )}
       <p>
         <label htmlFor="title">Title</label>
         <input id="title" type="text" name="title" required defaultValue={event ? event.title : ''}/>
