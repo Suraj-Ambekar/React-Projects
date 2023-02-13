6119feac17ea3ffe607b39be39b8655b68d3e042
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-13 11:07:17 +0530

    learned useFetcher hook

--- frontend/src/components/NewsletterSignup.js
+++ frontend/src/components/NewsletterSignup.js
@@ -1,15 +1,26 @@
 import classes from './NewsletterSignup.module.css';
+import { useFetcher } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function NewsletterSignup() {
+  const fetcher = useFetcher();
+  const {data, state} = fetcher;
+
+  useEffect(()=>{
+    if(state === 'idle' && data && data.message){
+      window.alert(data.message);
+    }
+  },[data,state]);
+
   return (
-    <form method="post" className={classes.newsletter}>
+    <fetcher.Form method="post" action="/newsletter" className={classes.newsletter}>
       <input
         type="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </fetcher.Form>
   );
 }
 
