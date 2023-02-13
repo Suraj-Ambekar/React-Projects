8a8a973c35ef39b79dd0fa43d851af3d05cfb12f
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-13 09:53:10 +0530

    learned how to delete data from back end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import Root from './components/Root'
 import HomePage from './pages/HomePage';
 import EventsPage, {loader as eventsLoader} from './pages/EventsPage';
-import EventDetailPage, {loader as eventDetailLoader} from './pages/EventDetailPage';
+import EventDetailPage, {loader as eventDetailLoader, action as deleteEventAction} from './pages/EventDetailPage';
 import NewEventPage, {action as newEventAction} from './pages/NewEventPage';
 import EditEventPage from './pages/EditEventPage';
 import EventsRootLayout from './pages/EventRootLayout';
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
           id: 'event-detail',
           loader: eventDetailLoader,
           children: [
-            {index: true, element: <EventDetailPage />},
+            {index: true, element: <EventDetailPage />, action: deleteEventAction},
             {path: 'edit', element: <EditEventPage />},
           ]
         },        
--- frontend/src/pages/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage.js
@@ -1,4 +1,4 @@
-import { useRouteLoaderData, json } from 'react-router-dom';
+import { useRouteLoaderData, json,redirect } from 'react-router-dom';
 
 import EventItem from '../components/EventItem';
 
@@ -31,4 +31,24 @@ export async function loader({ request, params }) {
   } else {
     return response;
   }
+}
+
+
+export async function action({params, request}){
+  const eventId = params.eventId;
+
+  const response = fetch('http://localhost:8080/events/'+ eventId, {
+    method: request.method,
+  });
+
+  if (!response.ok) {
+    throw json(
+      { message: 'Could not delete event.' },
+      {
+        status: 500,
+      }
+    );
+  }
+
+  return redirect('/events');
 }
