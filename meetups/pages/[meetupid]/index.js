8da11f2bfc7a9878ac4aea1a1d9009761b683766
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-15 12:05:24 +0530

    added details page and styling to it.

---
+++ meetups/pages/[meetupid]/index.js
@@ -0,0 +1,15 @@
+import MeetupDetail from '@/components/meetups/MeetupDetail'
+import React, { Fragment } from 'react'
+
+const MeetupDetails = () => {
+  return (
+    < MeetupDetail
+        image= 'https://www.infront.com/wp-content/uploads/2015/08/meet-up.png.webp'
+        title= 'A first meetup'
+        address= 'lakjdf laksome ojdl aofsl'
+        description= 'lkdjf'
+    />
+  )
+}
+
+export default MeetupDetails
