9180184642ab028c63d2497bb9105b7850462a3d
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-16 08:59:27 +0530

    added css transtion effection

--- react-animations/src/App.js
+++ react-animations/src/App.js
@@ -7,7 +7,8 @@ import List from "./components/List/List";
 
 class App extends Component {
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBlock: false
   }
 
   showModal = () => {
@@ -22,8 +23,28 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
-        <Backdrop show={this.state.modalIsOpen} />
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState(prevState => ({ showBlock: !prevState.showBlock }))}
+        >
+          Toggle
+        </button>
+        <br />
+        {this.state.showBlock ? 
+          <div 
+            style={{
+              backgroundColor: "red",
+              width: 100,
+              height: 100,
+              margin: 'auto',
+              // textAlign: "center",
+            }}
+          /> :
+          null
+        }
+        {/* {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} : null/>
+        {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} : null /> */}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
--- react-animations/src/components/Backdrop/Backdrop.js
+++ react-animations/src/components/Backdrop/Backdrop.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import './Backdrop.css';
 
-const backdrop = (props) => {
+const Backdrop = (props) => {
     const cssClasses = ['Backdrop', props.show ? 'BackdropOpen' : 'BackdropClosed'];
 
     return <div className={cssClasses.join(' ')}></div>;
 };
 
-export default backdrop;
+export default Backdrop;
