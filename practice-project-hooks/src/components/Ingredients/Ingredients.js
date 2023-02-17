c28fb000533668796cb52d4dd2d10e1f866f4b62
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-17 15:54:53 +0530

    added get method to fetch the data from firebase

--- practice-project-hooks/src/components/Ingredients/Ingredients.js
+++ practice-project-hooks/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -6,6 +6,22 @@ import IngredientList from './IngredientList';
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
+
+  useEffect(()=> {
+    fetch('https://practice-project-hooks-default-rtdb.firebaseio.com/ingredients.json')
+      .then(response=> response.json())
+      .then(responseData => {
+        const loadedIngredients = [];
+        for (const key in responseData){
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      })
+  },[]);
 
 function addIngredientHandler(ingredient) {
   fetch('https://practice-project-hooks-default-rtdb.firebaseio.com/ingredients.json', {
