abf9acf54e6a746e4bcaebb711d0adad3c39f65e
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-18 07:44:48 +0530

    added useEffect clean up function

--- practice-project-hooks/src/components/Ingredients/Search.js
+++ practice-project-hooks/src/components/Ingredients/Search.js
@@ -9,7 +9,7 @@ const Search = React.memo(props => {
   const inputRef = useRef('');
 
   useEffect(() => {
-    setTimeout(()=>{
+    const timer = setTimeout(()=>{
       if (enteredFilter === inputRef.current.value) {
         const query =
           enteredFilter.length === 0
@@ -31,7 +31,9 @@ const Search = React.memo(props => {
 
       }
     },500);
-    
+    return () => {
+      clearTimeout(timer);
+    }
   }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
