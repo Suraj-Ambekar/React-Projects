871a3103218e9ee01d8f9c680bc99c74b7fcc155
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-18 10:09:13 +0530

    added useReducer succeffully and removed useState

--- practice-project-hooks/src/components/Ingredients/Ingredients.js
+++ practice-project-hooks/src/components/Ingredients/Ingredients.js
@@ -18,8 +18,24 @@ const ingredientReducer = (currentIngredients, action) => {
   }
 };
 
+const httpReducer = (curHttpState,action) => {
+  switch(action.type){
+    case 'SEND':
+      return {loading: true, error : null};
+    case 'RESPONSE':
+      return {...curHttpState, loading: false};
+    case 'ERROR':
+      return {loading: false, error: action.errorMessage}; 
+    case 'CLEAR':
+      return {...curHttpState, error: null}
+    default:
+      throw new Error("something not be reached")
+  }
+}
+
 const Ingredients = () => {
   const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  const [httpState, dispatchHttp] = useReducer(httpReducer, {loading: false, error: null});
   // const [userIngredients, setUserIngredients] = useState([]);
   // const [isLoading, setIsLoading] = useState(false);
   // const [error, setError] = useState();
@@ -35,14 +51,16 @@ const Ingredients = () => {
   }, []);
 
   const addIngredientHandler = ingredient => {
-    setIsLoading(true);
+    // setIsLoading(true);
+    dispatchHttp({type:'SEND'});
     fetch('https://practice-project-hooks-default-rtdb.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' }
     })
       .then(response => {
-        setIsLoading(false)
+        // setIsLoading(false)
+        dispatchHttp({type:'RESPONSE'});
         return response.json();
       })
       .then(responseData => {
@@ -55,30 +73,34 @@ const Ingredients = () => {
   };
 
   const removeIngredientHandler = ingredientId => {
-    setIsLoading(true);
+    // setIsLoading(true);
+    dispatchHttp({type:'SEND'});
     fetch(`https://practice-project-hooks-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`, {
       method: 'DELETE',
     }).then(response=> {
-      setIsLoading(false);
+      // setIsLoading(false);
+      dispatchHttp({type:'RESPONSE'});
     //   setUserIngredients(prevIngredients =>
     //   prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
     // );
     dispatch({type: 'DELETE', id: ingredientId});
     }).catch(error => {
-      setError('Someting went Wrong');
-      setIsLoading(false);
+      // setError('Someting went Wrong');
+      // setIsLoading(false);
+      dispatchHttp({type:'ERROR', errorMessage: "Something went wrong"});
     })
   };
 
   const clearError = () => {
-    setError(null);
+    // setError(null);
+    dispatchHttp({type:'CLEAR'});
     
   }
 
   return (
     <div className="App">
-      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
-      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
+      {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={httpState.loading}/>
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
